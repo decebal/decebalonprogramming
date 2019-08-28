b461187a34dbe68bf937f8b6cbe03f21970bc661
Decebal Dobrica <decebal.dobrica@tellimer.com>
2019-08-28 09:48:38 +0100

    production blog

--- src/components/BlogIndexPage.js
+++ src/components/BlogIndexPage.js
@@ -32,21 +32,22 @@ function BlogIndexPage({ blogRoot, pageCount, pageNumber, postRoutes }) {
       }
       <footer className={styles.footer}>
         <div>
-          <a
-            href='./rss.xml'
-            target='_blank'
-            style={{ float: 'right' }}>
-            RSS
-          </a>
+          {/*<a*/}
+          {/*  href='./rss.xml'*/}
+          {/*  target='_blank'*/}
+          {/*  style={{ float: 'right' }}>*/}
+          {/*  RSS*/}
+          {/*</a>*/}
           <Link href='./about'>
             About
           </Link> &bull;{' '}
           <Link href='./tags'>
             Tags
-          </Link> &bull;{' '}
-          <a href='https://github.com/frontarm/create-react-blog'>
-            Source
-          </a>
+          </Link>
+          {/*&bull;{' '}*/}
+          {/*<a href='https://github.com/frontarm/create-react-blog'>*/}
+          {/*  Source*/}
+          {/*</a>*/}
         </div>
       </footer>
     </div>
