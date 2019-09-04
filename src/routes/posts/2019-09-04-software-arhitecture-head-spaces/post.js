ea0ef0921b1dd4eb3756bab295d853985a4bf550
Decebal Dobrica <decebal.dobrica@tellimer.com>
2019-09-04 11:42:29 +0100

    update about me page & new post about software architectures

---
+++ src/routes/posts/2019-09-04-software-arhitecture-head-spaces/post.js
@@ -0,0 +1,7 @@
+export default {
+  author: "Decebal Dobrica",
+  title: `Software Architecture Head Spaces`,
+  tags: ["software-architecture", "evolutionary-architecture", "serverless2.0", "DDD", "graphql"],
+  spoiler: "Topics in Software Architecture I am currently pondering and distilling.",
+  getContent: () => import('./document.mdx'),
+}
