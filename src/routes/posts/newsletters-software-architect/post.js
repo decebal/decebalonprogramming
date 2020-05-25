e6f0c587fc87ed82cda5db47c2fc965390667553
Decebal Dobrica <decebal.dobrica@tellimer.com>
2020-05-25 02:53:43 +0100

    chore: fix typos

--- src/routes/posts/newsletters-software-architect/post.js
+++ src/routes/posts/newsletters-software-architect/post.js
@@ -2,9 +2,9 @@ export default {
   author: "Decebal D.",
   title: "How do I stay updated as a Software Architect",
   tags: ["news", "newsletter", "software architecture", "books"],
-  spoiler: "Staying up to date with technology developments is an important of a modern software \n" +
-    "developer's daily activity, as well as one of the defining behaviours for a successful\n" +
-    "career. This is my list of newsletter that I am grateful for.",
+  spoiler: "Staying up to date with technology developments is an important part of a modern software \n" +
+    "developer's daily activity as well as one of the defining behaviours for a successful\n" +
+    "career. This is my list of newsletters that I am grateful for.",
   date: new Date("2020-05-24T15:00:00+00:00"),
   getContent: () => import('./document.mdx'),
 }
