717a98934b16f4766f8b4527f366b5a826b9fa0a
Decebal Dobrica <decebal.dobrica@tellimer.com>
2020-06-05 23:56:13 +0100

    docs: wip youtube channels

---
+++ src/routes/posts/2020-06-05-top-21-youtube-channels-for-software-developers/post.js
@@ -0,0 +1,9 @@
+export default {
+  author: "Decebal D.",
+  title: "Top 21 Youtube Channels for Software Developers",
+  tags: ["k8s", "golang", "typescript", "kubernetes", "resources"],
+  spoiler: "Youtube channels that bring value towards a software developer's education and help stay in touch with developments, biased by typescript and goLang programming languages",
+  getContent: () => import('./document.mdx'),
+}
+
+
