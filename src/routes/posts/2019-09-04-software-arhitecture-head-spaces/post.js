f80c09bd6c23e70b30654982160cc7f0a10f5840
Decebal Dobrica <decebal.dobrica@tellimer.com>
2020-07-18 11:03:52 +0100

    article: organise channels in categories

--- src/routes/posts/2019-09-04-software-arhitecture-head-spaces/post.js
+++ src/routes/posts/2019-09-04-software-arhitecture-head-spaces/post.js
@@ -1,7 +1,8 @@
 export default {
-  author: "Decebal Dobrica",
+  author: "Decebal D.",
   title: `Software Architecture Head Spaces`,
   tags: ["software-architecture", "evolutionary-architecture", "serverless2.0", "DDD", "graphql"],
   spoiler: "Topics in Software Architecture I am currently pondering and distilling.",
   getContent: () => import('./document.mdx'),
+  date: new Date("2019-09-04T06:00:00"),
 }
--- src/routes/posts/2020-07-18-top-39-youtube-channels-for-software-developers/post.js
+++ src/routes/posts/2020-07-18-top-39-youtube-channels-for-software-developers/post.js
@@ -1,9 +1,8 @@
 export default {
   author: "Decebal D.",
-  title: "Top 21 Youtube Channels for Software Developers",
+  title: "Top 39 Youtube Channels for Software Developers",
   tags: ["k8s", "golang", "typescript", "kubernetes", "resources"],
   spoiler: "Youtube channels that bring value towards a software developer's education and help stay in touch with developments, biased by typescript and goLang programming languages",
   getContent: () => import('./document.mdx'),
+  date: new Date("2020-07-18T06:00:00"),
 }
-
-
