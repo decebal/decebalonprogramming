f52b3c53c05623b5e5eeb2353bc4760d1a9ad70e
decebal <decebal1988@gmail.com>
2022-06-13 22:54:44 +0100

    docs: snowflake article

---
+++ src/routes/posts/2022-06-13-nodejs-snowflake-sdk-optimisations-for-aws-lambda/post.js
@@ -0,0 +1,10 @@
+export default {
+  author: "Decebal D.",
+  title: "Nodejs Snowflake Sdk Optimisations for Aws Lambda",
+  tags: ["snowflake", "serverless", "typescript", "aws", "aws lambda"],
+  spoiler: "Using Snowflake as an analytics pool consumed by aws lambda.",
+  getContent: () => import('./document.mdx'),
+  date: new Date("2022-06-14T06:00:00"),
+}
+
+
